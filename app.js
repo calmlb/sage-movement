89449631597d410936a11569baeb871cc4c25869
Eric Mathias <emathias@gmail.com>
2021-01-09 20:52:23 -0500

    express server, mongo, mongoose, and routes defined. ejs started

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var blogRouter = require('./routes/blog');
+var blogPostsRouter = require('./routes/blogPosts');
+var coursesRouter = require('./routes/courses');
+var liveCourseRouter = require('./routes/live');
+var onDemandRouter = require('./routes/onDemand');
+var workshopsRouter = require('./routes/workshops');
 
 var app = express();
 
@@ -21,6 +28,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/blog', blogRouter);
+app.use('/blogPosts', blogPostsRouter);
+app.use('/courses', coursesRouter);
+app.use('/live', liveCourseRouter);
+app.use('/onDemand', onDemandRouter);
+app.use('/workshops', workshopsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/live.js
+++ routes/live.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sage Movement' });
+  res.send('choose your live course');
 });
 
 module.exports = router;
